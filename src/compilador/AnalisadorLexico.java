e60f8c2a772be25a3141a85e4e19005acc48d097
aqvale <anderson_qdv@hotmail.com>
2016-07-26 15:20:02 -0300

    Completo

--- src/compilador/AnalisadorLexico.java
+++ src/compilador/AnalisadorLexico.java
@@ -27,7 +27,8 @@ public class AnalisadorLexico {
         char num;
         for (int i = 0; i < in.length(); i++) {
             num = in.charAt(i);
-            if (numero(num) || num == '-' || num == '.' || num == ';') {
+            if (numero(num) || num == '-' || num == '.' || num == ';'
+                    || num =='+' || num == '/' || num == '*') {
                 switch (estado) {
                     case ("primeiro"):
                         if (numero(num)) {
@@ -44,7 +45,8 @@ public class AnalisadorLexico {
                             estado = "segundo";
                         } else if (num == '.') {
                             estado = "decimal";
-                        } else if (num == ';') {
+                        } else if (num == ';' || num == '+' || num == '-' 
+                                || num == '*' || num == '/') {
                             estado = "delimitador";
                         } else {
                             estado = "erro";
@@ -94,15 +96,165 @@ public class AnalisadorLexico {
             return false;
         
     }
-
+    
+    public boolean automatoPalavraReservada (String in){
+        for (int i = 0; i < palavrasReservadas.length; i++)
+            if(in.equals(palavrasReservadas[i]))
+                return true;         
+            
+        return false;       
+    }
+    
+    public boolean automatoIdentificador (String in){
+        String estado = "primeiro";
+        char c;
+        
+        for(int i = 0; i < in.length(); i++){
+            c = in.charAt(i);
+            switch(estado){
+                case ("primeiro"):
+                    if(letra(c)){
+                        estado = "segundo";
+                    } else {
+                        estado = "erro";
+                    }
+                    break;
+                    
+                case ("segundo"):
+                    if(letra(c) || numero(c) || c == '_'){
+                        estado = "segundo";
+                    } else if(c == ';'){
+                        estado = "delimitador";
+                    } else {
+                        estado = "erro";
+                    }                    
+                    break;
+                
+                case ("erro"):
+                    return false;
+            }            
+        }
+        
+        if(estado.equals("delimitador")){
+            return true;
+        } else {
+            return false;
+        }
+            
+    }
+    
+    public boolean automatoCadeia(String in){
+        String estado = "primeiro";
+        char c;
+        for(int i = 0; i < in.length(); i++){
+            c = in.charAt(i);
+            
+            switch(estado){
+                case ("primeiro"):
+                    if(c == '\"'){
+                        estado = "segundo";
+                    } else {
+                        estado = "erro";
+                    }                      
+                    break;
+                
+                case ("segundo"):
+                    if(letra(c)){
+                        estado = "terceiro";
+                    } else {
+                        estado = "erro";
+                    }                    
+                    break;
+                    
+                case ("terceiro"):
+                    if(c == '\"'){
+                        estado = "delimitador";
+                    } else if(c == '\\') {
+                        estado = "erro";
+                    } else {
+                        estado = "terceiro";
+                    }                    
+                    break;
+                
+                case ("erro"):
+                    return false;
+            }
+        }
+        
+        if(estado.equals("delimitador"))
+            return true;
+        else 
+            return false;
+        
+    }
+    
+    public boolean automatoCaracter(String in){
+        String estado = "primeiro";
+        char c;
+        for(int i = 0; i < in.length(); i++){
+            c = in.charAt(i);
+            
+            switch(estado){
+                case ("primeiro"):
+                    if(c == '\''){
+                        estado = "segundo";
+                    } else {
+                        estado = "erro";
+                    }                      
+                    break;
+                
+                case ("segundo"):
+                    if(letra(c) || numero(c)){
+                        estado = "terceiro";
+                    } else {
+                        estado = "erro";
+                    }                    
+                    break;
+                    
+                case ("terceiro"):
+                    if(c == '\''){
+                        estado = "delimitador";
+                    } else {
+                        estado = "erro";
+                    }                    
+                    break;
+                
+                case ("erro"):
+                    return false;
+            }
+        }
+        
+        if(estado.equals("delimitador"))
+            return true;
+        else 
+            return false;
+        
+    }
+    
+    
     public boolean numero(char x) {
-        String numero = "1234567890";
         for (int i = 0; i < numero.length(); i++) {
             if (x == numero.charAt(i)) {
                 return true;
             }
         }
         return false;
+    }
+    
+    public boolean letra (char x){
+      for (int i=0;i<letras.length();i++){
+        if(x == letras.charAt(i)) 
+            return true;
+      }
+      return false;
+    }
+    
+    public boolean delimitador (char x){
+      for (int i=0;i<delimitador.length();i++){
+        if(x == delimitador.charAt(i)) 
+            return true;
+      }
+      return false;
     }
 
     /**
@@ -113,11 +265,27 @@ public class AnalisadorLexico {
         
         AnalisadorLexico analisadorLexico = new AnalisadorLexico();
         
-        teste = analisadorLexico.automatoNumero("10111111.1;");
+        teste = analisadorLexico.automatoIdentificador("qa123_23a");
         if(teste)
             System.out.println("Verdadeiro");
         else 
             System.out.println("Erro");
+        
+        teste = analisadorLexico.automatoCadeia("\"quaseuan cj 13j 123j svj12\" ");
+        
+        if(teste)
+            System.out.println("Verdadeiro");
+        else 
+            System.out.println("Erro");
+        
+        teste = analisadorLexico.automatoCaracter("\'1\' ");
+        
+        if(teste)
+            System.out.println("Verdadeiro");
+        else 
+            System.out.println("Erro");
+        
+        
     }
 
 }
